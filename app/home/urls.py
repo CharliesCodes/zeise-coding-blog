be86fe88a9ac288c58b59cd581814461d9eb1d9f
Dominique Zeise <d.zei@gmx.de>
2022-04-21 13:30:42 +0200

    cloudinary profile image on home

--- app/home/urls.py
+++ app/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('', views.home_page_view, name='home'),
     path('about/', views.about_page_view, name='about'),
     path('impressum/', views.impressum_page_view, name='impressum'),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 from members.models import Profile
 from django.shortcuts import render
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+import cloudinary
+
+# class HomePageView(TemplateView):
+#     template_name = 'home.html'
+
+def home_page_view(request):
+    return render(request, 'home.html')
+
 
 def about_page_view(request):
     admin_profile = get_object_or_404(Profile, id=1)
