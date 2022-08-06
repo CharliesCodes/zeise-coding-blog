bb24ec6e0015649c7fcac8e206378c0d8c31fc95
Dominique Zeise <d.zei@gmx.de>
2022-08-06 15:49:27 +0200

    responsive design

--- app/blog/feeds.py
+++ app/blog/feeds.py
@@ -2,11 +2,14 @@ from django.contrib.syndication.views import Feed
 from django.template.defaultfilters import truncatewords
 from .models import Post
 from django.urls import reverse
+# from django.utils.feedgenerator import Atom1Feed
 
 class BlogFeed(Feed):
     title = "Latest Post"
     link = "/blog/"
     description = "Latest blog posts"
+    # Create Atom Feed instead of RSS
+    # feed_type = Atom1Feed
 
     def items(self):
         return Post.objects.filter(status=1)
