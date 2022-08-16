3333b6675db4db0120bd4f718490f4a21bffebff
Dominique Zeise <d.zei@gmx.de>
2022-08-16 22:49:14 +0200

    adds courses page

--- app/courses/urls.py
+++ app/courses/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.CourseList.as_view(), name="courses-home"),
-
     path("category/<str:category>/", views.category_view, name="course-category"),
     # path("<slug:slug>/", views.PostDetail.as_view(), name="course-detail"),
 
